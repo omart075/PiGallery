b196647a30e486f77404e2b4cb40b1c3bb705068
Oscar Martinez <omart0725@gmail.com>
2024-01-23 09:59:25 -0500

    Add event handler to control images

--- PiGallery_v2/pi-gallery/src/image_handler.js
+++ PiGallery_v2/pi-gallery/src/image_handler.js
@@ -1,50 +1,147 @@
 import React, { useEffect, useState } from "react";
-import img1 from './imgs/1.PNG'
-import img2 from './imgs/2.PNG'
-import img3 from './imgs/3.png'
+import img0 from './imgs/1.PNG'
+import img1 from './imgs/2.PNG'
+import img2 from './imgs/3.png'
+import img3 from './imgs/4.png'
+import img4 from './imgs/5.png'
+import img5 from './imgs/6.png'
+import img6 from './imgs/7.png'
+import gif0 from './imgs/2_1.gif'
+import gif1 from './imgs/2_2.gif'
 
-const images = [img1, img2, img3]
+const divisor = 2.5
+const image_info = [
+    {
+        img: img0,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img1,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img2,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img3,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img4,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img5,
+        width: 0,
+        height: 0
+    },
+    {
+        img: img6,
+        width: 0,
+        height: 0
+    },
+    {
+        img: gif0,
+        width: 0,
+        height: 0
+    },
+    {
+        img: gif1,
+        width: 0,
+        height: 0
+    }
+]
 
-export default function ImageSwapper() {
-    const [currentImage, setCurrentImage] = useState(null);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentImage(images[Math.floor(Math.random() * images.length)]);
-        }, 5000)
-        
-        return () => clearInterval(intervalId);
-    }, [])
-
+function resizeImages() {
     var ratio;
     var resized_height;
     var resized_width;
-    const divisor = 2.5
 
-    var wh = window.innerHeight
-    var ww = window.innerWidth
+    const wh = window.innerHeight
+    const ww = window.innerWidth
 
-    var img = new Image();
-    img.src = currentImage;
+    for (let i = 0; i < image_info.length; i++) {
+        var img = new Image();
+        img.src = image_info[i].img
 
-    if (img.width > img.height) {
-        ratio = img.width / ww
-        resized_width = ww / divisor
-        resized_height = img.height / ratio /divisor
+        if (img.width > img.height) {
+            ratio = img.width / ww
+            resized_width = ww / divisor
+            resized_height = img.height / ratio /divisor
+        }
+        else if (img.height > img.width) {
+            ratio = img.height / wh
+            resized_width = img.width / ratio / divisor
+            resized_height = wh / divisor
+        }
+        else {
+            resized_width = ww / divisor
+            resized_height = wh / divisor
+        }
+
+        image_info[i].width = resized_width
+        image_info[i].height = resized_height
     }
-    else if (img.height > img.width) {
-        ratio = img.height / wh
-        resized_width = img.width / ratio / divisor
-        resized_height = wh / divisor
-    }
-    else {
-        resized_width = ww / divisor
-        resized_height = wh / divisor
+    console.log(image_info)
+}
+
+export default function ImageSwapper() {
+    if (image_info[0].width === 0) {
+        resizeImages()
     }
 
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [currentImage, setCurrentImage] = useState(null);
+    const [random, setRandom] = useState(true);
+    const [key, setKey] = useState("");
+
+    useEffect(() => {
+        window.addEventListener('keypress', e => {
+            if (e.key === "s") {
+                setRandom(prev => !prev);
+                setKey(e.key)
+            }
+            else {
+                setRandom(false)
+                setKey(e.key)   
+            }
+        });
+    }, [])
+
+    useEffect(() => {
+        var intervalId = null;
+        if(random === true) {
+            console.log(random, key)
+            intervalId = setInterval(() => {
+                var i = Math.floor(Math.random() * image_info.length)
+                setCurrentIndex(i)
+                setCurrentImage(image_info[i]);
+            }, 5000)
+        }
+        else {
+            console.log(random, key)
+            intervalId = setInterval(() => {
+                if(currentIndex === image_info.length - 1) {
+                    setCurrentIndex(0);
+                } 
+                else {
+                     setCurrentIndex(currentIndex + 1);
+                }
+            }, 5000)
+        }
+        
+        return () => clearInterval(intervalId);
+    }, [random, key])
+    
     return (
         <div>
-            <img src={currentImage} style={{height: resized_height+'px', width:resized_width+'px'}}/>
+               {currentImage && <img src={currentImage.img} style={{height: currentImage.height+'px', width:currentImage.width+'px'}}/>}
         </div>
-    )
-}
+     )
+ }
