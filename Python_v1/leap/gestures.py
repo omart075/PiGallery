18cc449967be78de8c802983e9976855cf3cc395
Oscar Martinez <omart0725@gmail.com>
2024-02-01 10:12:41 -0500

    Improve gestures by introducing persistent hand tracking

--- Python_v1/leap/gestures.py
+++ Python_v1/leap/gestures.py
@@ -4,23 +4,60 @@ import pyautogui
 from pynput.keyboard import Controller
 
 '''
-sudo systemctl start libtrack_server
+Leap tracking server commands:
+
+sudo systemctl <start/stop/status/restart> libtrack_server
 '''
 
 pyautogui.PAUSE = 0
+pyautogui.FAILSAFE = False
 
 class MyListener(leap.Listener):
     def __init__(self):
         self.starting_x = None
         self.ending_x = None
         self.swipe_direction = None
-        self.hand_ack = True
-        self.gesture_ack = False
         self.keyboard = Controller()
-        self.pinching_ack = False
-        self.particle_ack = False
         self.display_width = pyautogui.size().width
         self.display_height = pyautogui.size().height
+        self.hand = None
+        self.hand_type = None
+
+        # flags are for logging only
+        self.hand_ack = True
+        self.gesture_ack = False
+        self.particle_mode = False
+
+        self._pinching = False
+        self._grabbing = False
+
+    @property
+    def grabbing(self):
+        return self._grabbing
+    
+    @grabbing.setter
+    def grabbing(self, update):
+        if self._grabbing != update and update == True:
+            self._grabbing = update
+            # self.keyboard.press('s')
+            # self.keyboard.release('s')
+            print(f"Gesture: fist | Hand: {self.hand_type} | Position: {self.hand.palm.position.x} | Strength: {self.hand.grab_strength}")
+        else:
+            self._grabbing = update
+
+    @property
+    def pinching(self):
+        return self._pinching
+    
+    @pinching.setter
+    def pinching(self, update):
+        if self._pinching != update and update == True:
+            self._pinching = update
+            self.particle_mode = not self.particle_mode
+            # pyautogui.click()
+            print(f"Gesture: pinch | Hand: {self.hand_type} | Position: {self.hand.palm.position.x}")
+        else:
+            self._pinching = update
 
     def on_connection_event(self, event):
         print("Connected")
@@ -37,39 +74,32 @@ class MyListener(leap.Listener):
     def on_tracking_event(self, event):
         # reset if hands are not visible
         if len(event.hands) == 0 and self.hand_ack:
+            self.hand_ack = False
+
             self.starting_x = None
             self.ending_x = None
-            self.hand_ack = False
             self.gesture_ack = False
-            self.static_ack = False
             self.swipe_direction = None
             print("No hands detected")
 
         for hand in event.hands:
             self.hand_ack = True
-            hand_type = "left" if str(hand.type) == "HandType.Left" else "right"
+            self.hand_type = "left" if str(hand.type) == "HandType.Left" else "right"
+            self.hand = hand
 
             # check for static gestures
-            if hand.palm.position.x > -100 and hand.palm.position.x < 100 and not self.swipe_direction and not self.static_ack:
-                if hand.grab_strength > 0.8 and not self.particle_ack:
-                    self.static_ack = True
-                    self.keyboard.press('s')
-                    self.keyboard.release('s')
-                    print(f"Gesture: fist | Hand: {hand_type} | Position: {hand.palm.position.x} | Strength: {hand.grab_strength}")
+            if hand.palm.position.x > -60 and hand.palm.position.x < 60 and not self.swipe_direction:
+                if hand.grab_strength == 0:
+                    self.grabbing = False
+                    self.pinching = False
+                elif hand.grab_strength > 0.8 and not self.particle_mode:
+                    self.grabbing = True
 
-                else:
-                    thumb = hand.digits[0].distal.next_joint
-                    index_finger = hand.digits[1].distal.next_joint
-
-                    diff = list(map(abs, map(float.__sub__, thumb, index_finger)))
-
-                    if diff[0] < 20 and diff[1] < 20 and diff[2] < 20:
-                        self.static_ack = True
-                        self.particle_ack = not self.particle_ack
-                        pyautogui.click()
-                        print(f"Gesture: pinch | Hand: {hand_type} | Position: {hand.palm.position.x}")
+                # left hand pinch requires some adjustments due to inaccuracy
+                elif (self.hand_type == "left" and hand.pinch_distance - 10 < 10) or (self.hand_type == "right" and hand.pinch_distance < 10):
+                    self.pinching = True
             
-            if self.particle_ack:
+            if self.particle_mode:
                 x = 0
                 y = 0
                 tracking_range = 200
@@ -115,13 +145,13 @@ class MyListener(leap.Listener):
                     # translate gesture to keypress
                     if self.swipe_direction == "forward" and hand.palm.position.x < 0 and not self.gesture_ack:
                         self.ending_x = hand.palm.position.x
-                        print(f"Gesture: swipe | Hand: {hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                        print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
                         self.keyboard.press('d')
                         self.keyboard.release('d')
                         self.gesture_ack = True
                     elif self.swipe_direction == "back" and hand.palm.position.x > 0 and not self.gesture_ack:
                         self.ending_x = hand.palm.position.x
-                        print(f"Gesture: swipe | Hand: {hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                        print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
                         self.keyboard.press('a')
                         self.keyboard.release('a')
                         self.gesture_ack = True
