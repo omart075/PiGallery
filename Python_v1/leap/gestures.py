a5fad3a4f5b186737d4b5fe5541b7d4e6f677456
Oscar Martinez <omart0725@gmail.com>
2024-02-02 10:20:17 -0500

    Adjusting swiping gesture; wip

--- Python_v1/leap/gestures.py
+++ Python_v1/leap/gestures.py
@@ -14,9 +14,6 @@ pyautogui.FAILSAFE = False
 
 class MyListener(leap.Listener):
     def __init__(self):
-        self.starting_x = None
-        self.ending_x = None
-        self.swipe_direction = None
         self.keyboard = Controller()
         self.display_width = pyautogui.size().width
         self.display_height = pyautogui.size().height
@@ -26,10 +23,21 @@ class MyListener(leap.Listener):
         # flags are for logging only
         self.hand_ack = True
         self.gesture_ack = False
-        self.particle_mode = False
+        self.moving_mouse = False
 
+        # gesture related
+        self.starting_x = None
+        self.ending_x = None
+        self.swipe_direction = None
+        self.forward_swipe_threshold = 100
+        self.back_swipe_threshold = -100
+        self.tracking_range = 200
+
+        self.valid_gestures = ["", "grabbing", "pinching"]
         self._pinching = False
         self._grabbing = False
+
+        self.pinching_threshold = 10
 
     @property
     def grabbing(self):
@@ -38,12 +46,10 @@ class MyListener(leap.Listener):
     @grabbing.setter
     def grabbing(self, update):
         if self._grabbing != update and update == True:
-            self._grabbing = update
             # self.keyboard.press('s')
             # self.keyboard.release('s')
             print(f"Gesture: fist | Hand: {self.hand_type} | Position: {self.hand.palm.position.x} | Strength: {self.hand.grab_strength}")
-        else:
-            self._grabbing = update
+        self._grabbing = update
 
     @property
     def pinching(self):
@@ -52,12 +58,85 @@ class MyListener(leap.Listener):
     @pinching.setter
     def pinching(self, update):
         if self._pinching != update and update == True:
-            self._pinching = update
-            self.particle_mode = not self.particle_mode
+            self.moving_mouse = not self.moving_mouse
             # pyautogui.click()
             print(f"Gesture: pinch | Hand: {self.hand_type} | Position: {self.hand.palm.position.x}")
+        
+        self._pinching = update
+
+    def handle_static_gesture(self, gesture=""):
+        valid = gesture in self.valid_gestures
+
+        # reset gestures by opening hand at center
+        if valid and self.hand.grab_strength == 0:
+            self.grabbing = False
+            self.pinching = False
+        elif valid and (self.hand.grab_strength > 0.8 and not self.moving_mouse):
+            self.grabbing = True
+        # left hand pinch requires some adjustments due to inaccuracy
+        elif valid and ((self.hand_type == "left" and self.hand.pinch_distance - 10 < self.pinching_threshold) or (self.hand_type == "right" and self.hand.pinch_distance < self.pinching_threshold)):
+            self.pinching = True
+
+    def handle_moving_mouse(self):
+        x = 0
+        y = 0
+
+        # ratio = resolution/tracking range
+        # divide resolution by 2 in order to split the +- conversion
+        x_ratio = (self.display_width / 2) / self.tracking_range
+        y_ratio = (self.display_height / 2) / self.tracking_range
+
+        
+        if self.hand.palm.position.x < 0 and self.hand.palm.position.x >= -200:
+            hand_x = self.hand.palm.position.x + self.tracking_range
+            x = round(hand_x * x_ratio)
+        elif self.hand.palm.position.x == 0:
+            x = self.display_width / 2
+        elif self.hand.palm.position.x > 0 and self.hand.palm.position.x <= 200:
+            x = round((self.display_width / 2) + self.hand.palm.position.x * x_ratio)
+        
+        if self.hand.palm.position.z < 0 and self.hand.palm.position.z >= -200:
+            hand_z = self.hand.palm.position.z + self.tracking_range
+            y = round(hand_z * y_ratio)
+        elif self.hand.palm.position.z == 0:
+            y = self.display_height / 2
+        elif self.hand.palm.position.z > 0 and self.hand.palm.position.z <= 200:
+            y = round((self.display_height / 2) + self.hand.palm.position.z * y_ratio)
+
+        pyautogui.moveTo(x, y)
+
+    def handle_swiping(self):
+        # if gesture is starting
+        if self.swipe_direction == None:
+            # determine direction of gesture from starting position
+            if self.hand.palm.position.x > self.forward_swipe_threshold and self.handle_static_gesture("pinching"):
+                self.swipe_direction = "forward"
+                self.starting_x = self.hand.palm.position.x
+            elif self.hand.palm.position.x < self.back_swipe_threshold and self.handle_static_gesture("pinching"):
+                self.swipe_direction = "back"
+                self.starting_x = self.hand.palm.position.x
+
+            print("started")
+
         else:
-            self._pinching = update
+            if not self.handle_static_gesture("pinching"):
+                self.swipe_direction = None
+                print("stopped")
+            else:
+                print("swiping")
+                # translate gesture to keypress
+                # if self.swipe_direction == "forward" and self.hand.palm.position.x < self.back_swipe_threshold:
+                #     self.ending_x = self.hand.palm.position.x
+                #     print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                #     # self.keyboard.press('d')
+                #     # self.keyboard.release('d')
+                #     self.swipe_direction = None
+                # elif self.swipe_direction == "back" and self.hand.palm.position.x > self.forward_swipe_threshold:
+                #     self.ending_x = self.hand.palm.position.x
+                #     print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                #     # self.keyboard.press('a')
+                #     # self.keyboard.release('a')
+                #     self.swipe_direction = None
 
     def on_connection_event(self, event):
         print("Connected")
@@ -88,73 +167,13 @@ class MyListener(leap.Listener):
             self.hand = hand
 
             # check for static gestures
-            if hand.palm.position.x > -60 and hand.palm.position.x < 60 and not self.swipe_direction:
-                if hand.grab_strength == 0:
-                    self.grabbing = False
-                    self.pinching = False
-                elif hand.grab_strength > 0.8 and not self.particle_mode:
-                    self.grabbing = True
-
-                # left hand pinch requires some adjustments due to inaccuracy
-                elif (self.hand_type == "left" and hand.pinch_distance - 10 < 10) or (self.hand_type == "right" and hand.pinch_distance < 10):
-                    self.pinching = True
+            if self.swipe_direction == None and self.hand.palm.position.x > -60 and self.hand.palm.position.x < 60:
+                self.handle_static_gesture()
             
-            if self.particle_mode:
-                x = 0
-                y = 0
-                tracking_range = 200
-
-                # ratio = res/tracking range
-                # divide res by 2 in order to split the +- conversion
-                x_ratio = (self.display_width / 2) / tracking_range
-                y_ratio = (self.display_height / 2) / tracking_range
-
-                
-                if hand.palm.position.x < 0 and hand.palm.position.x >= -200:
-                    hand_x = hand.palm.position.x + tracking_range
-                    x = round(hand_x * x_ratio)
-                elif hand.palm.position.x == 0:
-                    x = self.display_width / 2
-                elif hand.palm.position.x > 0 and hand.palm.position.x <= 200:
-                    x = round((self.display_width / 2) + hand.palm.position.x * x_ratio)
-                
-                if hand.palm.position.z < 0 and hand.palm.position.z >= -200:
-                    hand_z = hand.palm.position.z + tracking_range
-                    y = round(hand_z * y_ratio)
-                elif hand.palm.position.z == 0:
-                    y = self.display_height / 2
-                elif hand.palm.position.z > 0 and hand.palm.position.z <= 200:
-                    y = round((self.display_height / 2) + hand.palm.position.z * y_ratio)
-
-                pyautogui.moveTo(x, y)
-
-            else:
-                # if gesture is starting
-                if not self.swipe_direction:
-                    # determine direction of gesture from starting position
-                    if hand.palm.position.x > 100:
-                        self.swipe_direction = "forward"
-                        self.starting_x = hand.palm.position.x
-
-                    elif hand.palm.position.x < -100:
-                        self.swipe_direction = "back"
-                        self.starting_x = hand.palm.position.x
-
-                # if gesture is ending
-                else:
-                    # translate gesture to keypress
-                    if self.swipe_direction == "forward" and hand.palm.position.x < 0 and not self.gesture_ack:
-                        self.ending_x = hand.palm.position.x
-                        print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
-                        self.keyboard.press('d')
-                        self.keyboard.release('d')
-                        self.gesture_ack = True
-                    elif self.swipe_direction == "back" and hand.palm.position.x > 0 and not self.gesture_ack:
-                        self.ending_x = hand.palm.position.x
-                        print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
-                        self.keyboard.press('a')
-                        self.keyboard.release('a')
-                        self.gesture_ack = True
+            if self.moving_mouse:
+                self.handle_moving_mouse()
+            # else:
+            #     self.handle_swiping()
 
 
 def main():
