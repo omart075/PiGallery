cf75137cbdf88c24c4a91fa9a2a59fc690bf54dd
Oscar Martinez <omart0725@gmail.com>
2024-02-05 09:48:14 -0500

    Fixed gestures

--- Python_v1/leap/gestures.py
+++ Python_v1/leap/gestures.py
@@ -17,27 +17,23 @@ class MyListener(leap.Listener):
         self.keyboard = Controller()
         self.display_width = pyautogui.size().width
         self.display_height = pyautogui.size().height
+
         self.hand = None
         self.hand_type = None
+        self.hand_detected = True
 
-        # flags are for logging only
-        self.hand_ack = True
-        self.gesture_ack = False
         self.moving_mouse = False
-
-        # gesture related
         self.starting_x = None
         self.ending_x = None
         self.swipe_direction = None
+        self.tracking_range = 200
         self.forward_swipe_threshold = 100
         self.back_swipe_threshold = -100
-        self.tracking_range = 200
+        self.pinching_threshold = 10
 
         self.valid_gestures = ["", "grabbing", "pinching"]
         self._pinching = False
         self._grabbing = False
-
-        self.pinching_threshold = 10
 
     @property
     def grabbing(self):
@@ -46,8 +42,8 @@ class MyListener(leap.Listener):
     @grabbing.setter
     def grabbing(self, update):
         if self._grabbing != update and update == True:
-            # self.keyboard.press('s')
-            # self.keyboard.release('s')
+            self.keyboard.press('s')
+            self.keyboard.release('s')
             print(f"Gesture: fist | Hand: {self.hand_type} | Position: {self.hand.palm.position.x} | Strength: {self.hand.grab_strength}")
         self._grabbing = update
 
@@ -59,7 +55,7 @@ class MyListener(leap.Listener):
     def pinching(self, update):
         if self._pinching != update and update == True:
             self.moving_mouse = not self.moving_mouse
-            # pyautogui.click()
+            pyautogui.click()
             print(f"Gesture: pinch | Hand: {self.hand_type} | Position: {self.hand.palm.position.x}")
         
         self._pinching = update
@@ -71,11 +67,15 @@ class MyListener(leap.Listener):
         if valid and self.hand.grab_strength == 0:
             self.grabbing = False
             self.pinching = False
-        elif valid and (self.hand.grab_strength > 0.8 and not self.moving_mouse):
+        elif valid and self.hand.grab_strength > 0.8 and not self.moving_mouse:
+            if gesture == "grabbing":
+                return True
             self.grabbing = True
         # left hand pinch requires some adjustments due to inaccuracy
         elif valid and ((self.hand_type == "left" and self.hand.pinch_distance - 10 < self.pinching_threshold) or (self.hand_type == "right" and self.hand.pinch_distance < self.pinching_threshold)):
             self.pinching = True
+
+        return False
 
     def handle_moving_mouse(self):
         x = 0
@@ -109,34 +109,29 @@ class MyListener(leap.Listener):
         # if gesture is starting
         if self.swipe_direction == None:
             # determine direction of gesture from starting position
-            if self.hand.palm.position.x > self.forward_swipe_threshold and self.handle_static_gesture("pinching"):
+            if self.hand.palm.position.x > self.forward_swipe_threshold and self.handle_static_gesture("grabbing"):
                 self.swipe_direction = "forward"
                 self.starting_x = self.hand.palm.position.x
-            elif self.hand.palm.position.x < self.back_swipe_threshold and self.handle_static_gesture("pinching"):
+            elif self.hand.palm.position.x < self.back_swipe_threshold and self.handle_static_gesture("grabbing"):
                 self.swipe_direction = "back"
                 self.starting_x = self.hand.palm.position.x
 
-            print("started")
-
         else:
-            if not self.handle_static_gesture("pinching"):
+            # translate gesture to keypress
+            if self.swipe_direction == "forward" and self.hand.palm.position.x < self.back_swipe_threshold and not self.handle_static_gesture("grabbing"):
+                self.ending_x = self.hand.palm.position.x
+                print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                self.keyboard.press('d')
+                self.keyboard.release('d')
                 self.swipe_direction = None
-                print("stopped")
-            else:
-                print("swiping")
-                # translate gesture to keypress
-                # if self.swipe_direction == "forward" and self.hand.palm.position.x < self.back_swipe_threshold:
-                #     self.ending_x = self.hand.palm.position.x
-                #     print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
-                #     # self.keyboard.press('d')
-                #     # self.keyboard.release('d')
-                #     self.swipe_direction = None
-                # elif self.swipe_direction == "back" and self.hand.palm.position.x > self.forward_swipe_threshold:
-                #     self.ending_x = self.hand.palm.position.x
-                #     print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
-                #     # self.keyboard.press('a')
-                #     # self.keyboard.release('a')
-                #     self.swipe_direction = None
+            elif self.swipe_direction == "back" and self.hand.palm.position.x > self.forward_swipe_threshold and not self.handle_static_gesture("grabbing"):
+                self.ending_x = self.hand.palm.position.x
+                print(f"Gesture: swipe | Hand: {self.hand_type} | Direction: {self.swipe_direction} | Position: {self.starting_x} to {self.ending_x}")
+                self.keyboard.press('a')
+                self.keyboard.release('a')
+                self.swipe_direction = None
+            elif not self.handle_static_gesture("grabbing"):
+                self.swipe_direction = None
 
     def on_connection_event(self, event):
         print("Connected")
@@ -152,17 +147,16 @@ class MyListener(leap.Listener):
 
     def on_tracking_event(self, event):
         # reset if hands are not visible
-        if len(event.hands) == 0 and self.hand_ack:
-            self.hand_ack = False
+        if len(event.hands) == 0 and self.hand_detected:
+            self.hand_detected = False
 
             self.starting_x = None
             self.ending_x = None
-            self.gesture_ack = False
             self.swipe_direction = None
             print("No hands detected")
 
         for hand in event.hands:
-            self.hand_ack = True
+            self.hand_detected = True
             self.hand_type = "left" if str(hand.type) == "HandType.Left" else "right"
             self.hand = hand
 
@@ -170,10 +164,11 @@ class MyListener(leap.Listener):
             if self.swipe_direction == None and self.hand.palm.position.x > -60 and self.hand.palm.position.x < 60:
                 self.handle_static_gesture()
             
+            # check for dynamic gestures
             if self.moving_mouse:
                 self.handle_moving_mouse()
-            # else:
-            #     self.handle_swiping()
+            else:
+                self.handle_swiping()
 
 
 def main():
